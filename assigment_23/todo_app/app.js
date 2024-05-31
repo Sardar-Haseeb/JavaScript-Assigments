bce016e21bb0e48f340bab23cc7dcb398efbe8bf
Sardar-Haseeb <shr.sardarhaseeburrehman@gmail.com>
2024-05-31 22:37:51 +0500

    to_do
    basic todo application

--- assigment_23/todo_app/app.js
+++ assigment_23/todo_app/app.js
@@ -1,46 +1,124 @@
 
 
+// function addtodo() {
+//     var todoParent = document.getElementById("todoParent")
+//     var todoinput = document.getElementById("todoinput")
+
+//     if (todoinput.value.length < 3) {
+//         alert("eneter currect value")
+//         return
+//     }
+
+//     var li = document.createElement("li")
+//     li.innerHTML = todoinput.value
+
+//     // crete delete btn
+
+//     var editBtn = document.createElement("button")
+//     editBtn.innerHTML = "EDIT"
+//     var deltBtn = document.createElement("button")
+//     deltBtn.innerHTML = "DEL"
+
+//     li.appendChild(editBtn);
+//     li.appendChild(deltBtn);
+//     editBtn.setAttribute("onclick", "editBtn(this)")
+
+//     deltBtn.setAttribute("onclick", "delTodo(this)")
+//     // deltBtn.onclick = "delTodo()"
+
+
+
+//     todoParent.appendChild(li)
+//     todoinput.value = ""
+
+
+
+// }
+// function delTodo(ele) {
+//     console.log("delTodo", ele.parentNode)
+
+//     ele.parentNode.remove()
+// }
+
+
+// function deleteAll() {
+//     var todoParent = document.getElementById("todoParent")
+//     todoParent.innerHTML = " "
+// }
+
+
+// function editBtn(ele) {
+//     // ele.parentNode = "hello"
+
+//     var editVal = prompt("enter value", ele.previousSibling.nodeValue)
+//     console.log(editVal)
+//     ele.previousSibling.nodeValue = editVal
+// }   
+
+
+
+
+
 function addtodo() {
-    var todoParent = document.getElementById("todoParent")
-    var todoinput = document.getElementById("todoinput")
+    var todoParent = document.getElementById("todoParent");
+    var todoinput = document.getElementById("todoinput");
 
     if (todoinput.value.length < 3) {
-        alert("eneter currect value")
-        return
+        alert("Enter correct value");
+        return;
     }
 
-    var li = document.createElement("li")
-    li.innerHTML = todoinput.value
+    var li = document.createElement("li");
+    li.innerHTML = todoinput.value;
 
-    // crete delete btn
+    // Create button container
+    var buttonContainer = document.createElement("div");
+    buttonContainer.className = "button-container";
 
-    var editBtn = document.createElement("button")
-    editBtn.innerHTML = "EDIT"
-    var deltBtn = document.createElement("button")
-    deltBtn.innerHTML = "DEL"
+    // Create edit button
+    var editBtn = document.createElement("button");
+    editBtn.innerHTML = "EDIT";
+    editBtn.setAttribute("onclick", "editBtn(this)");
 
-    li.appendChild(editBtn);
-    li.appendChild(deltBtn);
+    // Create delete button
+    var deltBtn = document.createElement("button");
+    deltBtn.innerHTML = "DEL";
+    deltBtn.setAttribute("onclick", "delTodo(this)");
 
-    deltBtn.setAttribute("onclick", "delTodo(this)")
-    // deltBtn.onclick = "delTodo()"
+    // Append buttons to the container
+    buttonContainer.appendChild(editBtn);
+    buttonContainer.appendChild(deltBtn);
 
+    // Append button container to the li
+    li.appendChild(buttonContainer);
 
+    // Append li to the todoParent
+    todoParent.appendChild(li);
 
-    todoParent.appendChild(li)
-    todoinput.value = ""
-
-
-
+    // Clear the input
+    todoinput.value = "";
 }
+
 function delTodo(ele) {
-    console.log("delTodo", ele.parentNode)
-
-    ele.parentNode.remove()
+    console.log("delTodo", ele.parentNode.parentNode);
+    ele.parentNode.parentNode.remove();
 }
-
 
 function deleteAll() {
-    var todoParent = document.getElementById("todoParent")
-    todoParent.innerHTML = " "
+    var todoParent = document.getElementById("todoParent");
+    todoParent.innerHTML = "";
 }
+
+function editBtn(ele) {
+    var editVal = prompt("Enter value", ele.parentNode.parentNode.firstChild.nodeValue);
+    console.log(editVal);
+    ele.parentNode.parentNode.firstChild.nodeValue = editVal;
+}
+
+
+
+
+
+
+
+console.log("one"/2)
